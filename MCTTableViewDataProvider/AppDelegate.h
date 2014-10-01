28be58d1e66e6920a2e9c23540c8c35b4c1d24e7
Macostik <admin@iMac-admin.local>
2014-10-01 23:18:24 +0300

    Initial commit

---
+++ MCTTableViewDataProvider/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MCTTableViewDataProvider
+//
+//  Created by Macostik on 10/1/14.
+//  Copyright (c) 2014 Macostik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MCTTableViewDataProvider/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MCTTableViewDataProvider
+//
+//  Created by Macostik on 10/1/14.
+//  Copyright (c) 2014 Macostik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
